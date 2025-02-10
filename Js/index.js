79b744b76eb9ad22716214801bc6a3195bfa580f
Rahul <rajrahul00945@gmail.com>
2025-02-10 13:54:47 +0530

    Project Added

--- Js/index.js
+++ Js/index.js
@@ -1,29 +1,143 @@
-function themeToggle() {
-    const body = document.body;
-    const themeIcon = document.getElementById('theme-icon');
-  
-    // Toggle dark mode
-    body.classList.toggle('dark-mode');
-  
-    // Switch icon between moon and sun
-    if (body.classList.contains('dark-mode')) {
-      themeIcon.classList.replace('fa-moon', 'fa-sun');
-    } else {
-      themeIcon.classList.replace('fa-sun', 'fa-moon');
-    }
-  
-    // Save theme preference
-    localStorage.setItem('theme', body.classList.contains('dark-mode') ? 'dark' : 'light');
-  }
-  
-  // Load theme on page load
-  window.onload = function () {
-    const savedTheme = localStorage.getItem('theme');
-    const body = document.body;
-    const themeIcon = document.getElementById('theme-icon');
-  
-    if (savedTheme === 'dark') {
-      body.classList.add('dark-mode');
-      themeIcon.classList.replace('fa-moon', 'fa-sun');
-    }
-  }
+//Navigation bar effects on scroll
+window.addEventListener("scroll", function () {
+  const header = document.querySelector("header");
+  header.classList.toggle("sticky", this.window.scrollY > 0);
+})
+
+// services section - Modal
+const serviceModals = document.querySelectorAll(".service-modal");
+const learnmoreBtns = document.querySelectorAll(".learn-more-btn");
+const modalCloseBtns = document.querySelectorAll(".modal-close-btn");
+
+var modal = function (modalClick) {
+  serviceModals[modalClick].classList.add("active");
+}
+
+learnmoreBtns.forEach((learnmoreBtn, i) => {
+  learnmoreBtn.addEventListener("click", () => {
+      modal(i);
+  });
+});
+
+modalCloseBtns.forEach((modalCloseBtns) => {
+  modalCloseBtns.addEventListener("click", () => {
+      serviceModals.forEach((modalView) => {
+          modalView.classList.remove("active");
+      });
+  });
+});
+
+
+//Portfolio section - Modal
+
+
+//Our clients - Swiper
+
+//Website dark/light theme
+const themeBtn = document.querySelector(".theme-btn");
+
+themeBtn.addEventListener("click", () => {
+  document.body.classList.toggle("dark-theme");
+  themeBtn.classList.toggle("sun");
+
+  localStorage.setItem("saved-theme", getCurrentTheme());
+  localStorage.setItem("saved-icon", getCurrentIcon());
+});
+
+const getCurrentTheme = () => document.body.classList.contains("dark-theme") ? "dark" : "light";
+const getCurrentIcon = () => themeBtn.classList.contains("sun") ? "sun" : "moon";
+
+const savedTheme = localStorage.getItem("saved-theme");
+const savedIcon = localStorage.getItem("saved-icon");
+
+if (savedTheme) {
+  document.body.classList[savedTheme === "dark" ? "add" : "remove"]("dark-theme");
+  themeBtn.classList[savedIcon === "sun" ? "add" : "remove"]("sun");
+}
+
+//Scroll to top button
+const scrollTopBtn = document.querySelector(".scrollToTop-btn");
+window.addEventListener("scroll", function () {
+  scrollTopBtn.classList.toggle("active", window.scrollY > 500);
+});
+
+scrollTopBtn.addEventListener("click", () => {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+})
+
+//Navigation menu items active on page scroll
+window.addEventListener("scroll", () => {
+  const sections = document.querySelectorAll('section');
+  const scrollY = window.pageYOffset;
+
+  sections.forEach(current => {
+      let sectionHeight = current.offsetHeight;
+      let sectionTop = current.offsetTop - 50;
+      let id = current.getAttribute("id");
+
+      if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+          document.querySelector(".nav-items a[href*=" + id + "]").classList.add("active");
+      }
+      else {
+          document.querySelector(".nav-items a[href*='" + id + "']").classList.remove("active");
+      }
+  })
+})
+
+//Responsive navigation menu toggle
+
+const menuBtn = document.querySelector(".nav-menu-btn");
+const closeBtn = document.querySelector(".nav-close-btn");
+const navigation = document.querySelector(".navigation");
+const navItems = document.querySelectorAll(".nav-items a");
+
+menuBtn.addEventListener("click", () => {
+  navigation.classList.add("active");
+});
+
+closeBtn.addEventListener("click", () => {
+  navigation.classList.remove("active");
+});
+
+navItems.forEach((navItem) => {
+  navItem.addEventListener("click", () => {
+      navigation.classList.remove("active");
+  });
+});
+
+//Scroll reveal animations
+// Common reveal options to create reveal animations
+ScrollReveal({
+  // reset: true,
+  distance: '60px',
+  duration: 2500,
+  delay: 100
+});
+
+//Target elements, and specify options to create reveal animations
+
+ScrollReveal().reveal(' .home .info h2', { delay: 4500, origin: 'left' });
+ScrollReveal().reveal('.home .info h2', { delay: 4500, origin: 'left' });
+ScrollReveal().reveal('.home .info h3, .home .info p,.about-info .btn', { delay: 500, origin: 'right' });
+ScrollReveal().reveal('.home .info .btn', { delay: 400, origin: 'bottom' });
+ScrollReveal().reveal('.media-icons i, .contact-left li', { delay: 500, origin: 'left', interval: 200 });
+ScrollReveal().reveal('.home-img,about-img', { delay: 4000, origin: 'bottom' });
+ScrollReveal().reveal('.about .description, .copy-right', { delay: 600, origin: 'right' });
+ScrollReveal().reveal('.about .professional-list li', { delay: 500, origin: 'right', interval: 200 });
+ScrollReveal().reveal('.skills-description, .service-description, .contact-card, .client-swiper,contact-left h2', { delay: 700, origin: 'left' });
+ScrollReveal().reveal('.experience-card, .service-card, .education, .portfolio .img-card', { delay: 800, origin: 'bottom',interval:200 });
+ScrollReveal().reveal('footer .group',{delay:500,origin:'top',interval:200});
+
+
+//loader
+
+
+function loaderAnimation() {
+  var loader = document.querySelector("#loader")
+  setTimeout(function () {
+      loader.style.top = "-100%"
+  }, 4200)
+}
+loaderAnimation();
+
